d1b7250724fb455c324b241038dbb8a35c0b045b
Casper Lai <casper.lai@sleepingdesign.com>
2017-09-24 14:21:18 +0800

    Support auto complete

--- src/GorillaFacade.php
+++ src/GorillaFacade.php
@@ -4,6 +4,22 @@ namespace Gorilla\Laravel;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * Class GorillaFacade
+ *
+ * @package Gorilla\Laravel
+ *
+ * @method static \Gorilla\Client|\Gorilla\GraphQL\Query query(string $name)
+ * @method static \Gorilla\Client|\Gorilla\GraphQL\Mutation mutation(string $name)
+ * @method static \Gorilla\Request setBaseUri(string $uri)
+ * @method static \Gorilla\Client cache(string $seconds = null)
+ * @method static \Gorilla\Client setDefaultCacheSecond(string $seconds)
+ * @method static boolean isCacheEnabled()
+ * @method static int getCacheSeconds()
+ * @method static \GuzzleHttp\Psr7\Response\JsonResponse|string get()
+ *
+ * @see \Gorilla\Client
+ */
 class GorillaFacade extends Facade
 {
     protected static function getFacadeAccessor()
